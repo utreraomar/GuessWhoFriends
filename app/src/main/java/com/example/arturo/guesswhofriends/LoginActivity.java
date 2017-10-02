a8a0ad92768dba75247713e8137c7bd65f9e6768
arturo <artm94@hotmail.com>
2017-10-02 01:06:40 -0500

    Add Facebook login

--- app/src/main/java/com/example/arturo/guesswhofriends/LoginActivity.java
+++ app/src/main/java/com/example/arturo/guesswhofriends/LoginActivity.java
@@ -1,13 +1,50 @@
 package com.example.arturo.guesswhofriends;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private LoginButton btnLoginFacebook;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        btnLoginFacebook =(LoginButton) findViewById(R.id.btnLoginFacebook);
+
+        btnLoginFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+                finishAndRemoveTask ();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(), "Ocurrio un error al iniciar sesion", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
