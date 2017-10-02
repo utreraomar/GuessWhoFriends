3d43d7e08a1e1a8dcee65ab7b1be3d267587c30e
arturo <artm94@hotmail.com>
2017-10-02 00:54:20 -0500

    Add Facebook login

--- app/src/main/java/com/example/arturo/guesswhofriends/MainActivity.java
+++ app/src/main/java/com/example/arturo/guesswhofriends/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.arturo.guesswhofriends;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnPlay, btnAchievments, btnConfig;
+    private AccessTokenTracker tokenTracker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnAchievments = (Button) findViewById(R.id.btnAchievments);
+        btnConfig = (Button) findViewById(R.id.btnConfig);
+
+        updateToken(AccessToken.getCurrentAccessToken());
+
+        tokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                updateToken(currentAccessToken);
+            }
+        };
+
+
+    }
+
+    public void updateToken(AccessToken token){
+        if(token == null){
+            Intent LoginActivity = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(LoginActivity);
+        }
     }
 }
